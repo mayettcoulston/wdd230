533d5bdb564c01048105a50cb85d60815363aa6b
mayettcoulston <may20018@byui.edu>
2025-03-25 00:18:15 +0300

    Week 2 modifications

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const hamburgerElement = document.querySelector("#myButton");
+const navElement = document.querySelector("#animatedMe");
+
+hamburgerElement.addEventListener("click", function () {
+    navElement.classList.toggle("open")
+    //hamburgerElement.classList.toggle("open");
+});
