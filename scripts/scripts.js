7620470322ba77d92140eb14c6f9fa0fc12576c0
mayettcoulston <may20018@byui.edu>
2025-03-26 10:54:01 +0300

    Dark Mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,11 @@ hamburgerElement.addEventListener("click", function () {
     navElement.classList.toggle("open")
     hamburgerElement.classList.toggle("open");
 });
+
+const darkModeButton = document.getElementById("darkMode");
+const body = document.body;
+
+darkModeButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    darkModeButton.textContent = body.classList.contains("dark-mode") ? "☀️ Light Mode" : "🌙 Dark Mode";
+});
