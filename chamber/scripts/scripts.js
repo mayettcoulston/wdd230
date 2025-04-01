b151caf9748d6b5bf258551bd1701ec67c8a2ba5
mayettcoulston <may20018@byui.edu>
2025-04-01 21:11:50 +0300

    Week3 project commits

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,3 +5,22 @@ hamburgerElement.addEventListener("click", function () {
     navElement.classList.toggle("open")
     hamburgerElement.classList.toggle("open");
 });
+
+function checkLastVisit() {
+    const visitMessage = document.getElementById("visitMessage");
+    const lastVisit = localStorage.getItem("lastVisit");
+    const now = Date.now();
+    
+    if (!lastVisit) {
+        visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const daysBetween = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+        if (daysBetween < 1) {
+            visitMessage.textContent = "Back so soon! Awesome!";
+        } else {
+            visitMessage.textContent = `You last visited ${daysBetween} ${daysBetween === 1 ? "day" : "days"} ago.`;
+        }
+    }
+    localStorage.setItem("lastVisit", now);
+}
+checkLastVisit();
